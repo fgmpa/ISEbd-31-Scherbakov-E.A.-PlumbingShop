c52f363b7f0af05a6f4f2143c99bb000bb9ed58d
EvgeniScherbakov <sherbakov_ev@bk.ru>
2022-11-11 21:02:08 +0400

    lab7 base

--- PlumbingShop/PlumbingShopDatabaseImplement/Migrations/20220513235424_Add_MessageInfo.Designer.cs
+++ PlumbingShop/PlumbingShopDatabaseImplement/Migrations/20220513235424_Add_MessageInfo.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlumbingShopDatabaseImplement;
 
 namespace PlumbingShopDatabaseImplement.Migrations
 {
     [DbContext(typeof(PlumbingShopDatabase))]
-    partial class PlumbingShopDatabaseModelSnapshot : ModelSnapshot
+    [Migration("20220513235424_Add_MessageInfo")]
+    partial class Add_MessageInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
